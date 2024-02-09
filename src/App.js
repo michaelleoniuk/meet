5b7b56acd3f82e7058bc49aa1c87e4253bb95632
Michael Leoniuk <michael.leoniuk@gmail.com>
2024-02-09 17:56:59 +0100

    CSS updated with UI chage

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { InfoAlert } from './components/Alert';
 import { ErrorAlert } from './components/Alert';
 import { WarningAlert } from './components/Alert';
 import CityEventsChart from "./components/CityEventsChart";
+import EventGenresChart from "./components/EventGenresChart";
 
 const App = ()=> {
   const [events, setEvents] = useState([]);
@@ -52,6 +53,7 @@ const App = ()=> {
         setErrorAlert={setErrorAlert}
       />
       <div className="charts-container">
+        <EventGenresChart events={events} />
         <CityEventsChart allLocations={allLocations} events={events} />
       </div>
       <EventList events={events}/>
